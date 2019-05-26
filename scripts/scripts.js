7b14892a7a911bd35334773410d4e339a789d262
unknown <lugowski.k@gmail.com>
2019-05-26 22:32:36 +0200

    add animations

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,61 @@
+// code related to menu
+
+const menuItems = [...document.querySelectorAll('nav li a')];
+
+const goToSection = function () {
+    const whereToGo = '[data-section=' + this.classList[0] + ']';
+    const goToThisSection = document.querySelector(whereToGo).offsetTop;
+    console.log(goToThisSection);
+    $('body, html').animate({
+        scrollTop: goToThisSection
+    });
+}
+
+menuItems.forEach(item => item.addEventListener('click', goToSection));
+let scrollPosition = 0;
+const navHideShow = () => {
+
+    const position = document.body.getBoundingClientRect().top;
+    const positionSectionMyStack = document.querySelector('.my-stack').offsetTop;
+    const navHeight = document.querySelector('.hero-nav').offsetHeight;
+    const nav = document.querySelector('.hero-nav');
+
+    if (position > scrollPosition) {
+        nav.classList.add('visible');
+        scrollPosition = document.body.getBoundingClientRect().top;
+    }
+    else {
+        scrollPosition = document.body.getBoundingClientRect().top;
+        nav.classList.remove('visible');
+    }
+
+    if (window.scrollY >= positionSectionMyStack - navHeight) {
+        nav.classList.add('color');
+    } else {
+        nav.classList.remove('color');
+    }
+}
+window.addEventListener('scroll', navHideShow);
+
+// code related to my-stack
+
+const myStackAnimations = () => {
+    const desktopImg = document.querySelector('.desktop-wrapper .desktop-img-wrapper');
+    const desktopImgPosition = document.querySelector('.desktop-wrapper .desktop-img-wrapper').offsetTop;
+    const iphone = document.querySelector('.mobile-img-wrapper img');
+    const iphoneLeft = document.querySelector('.mobile-img-wrapper .iphone-left');
+    if (window.scrollY >= desktopImgPosition / 1.5) {
+        desktopImg.classList.add('animated')
+    }
+    if (window.scrollY >= desktopImgPosition * 1.2) {
+        iphone.classList.add('animated');
+        iphoneLeft.classList.add('animated');
+    }
+}
+
+window.addEventListener('scroll', myStackAnimations);
+
+// code related to projects
 const projects = [
     projectPages = [
         {
@@ -49,6 +107,16 @@ const projects = [
             type: 'react-app',
             name: 'app2',
             src: '#app2'
+        },
+        {
+            type: 'react-app',
+            name: 'app3',
+            src: '#app3'
+        },
+        {
+            type: 'react-app',
+            name: 'app4',
+            src: '#app4'
         }
     ]
 ]
@@ -56,7 +124,6 @@ const projects = [
 const divsClassProjects = [...document.querySelectorAll('.projects')];
 
 projects.forEach((project, index) => {
-
     for (let key in project) {
         const singleDiv = document.createElement('div');
         singleDiv.classList.add('project-page');
@@ -71,7 +138,6 @@ const allDivs = [];
 allDivs.push([...document.querySelectorAll('.landing-page')])
 allDivs.push([...document.querySelectorAll('.js-games')])
 allDivs.push([...document.querySelectorAll('.react-app')])
-console.log(allDivs);
 
 const createElements = () => {
     for (key in allDivs) {
@@ -81,6 +147,7 @@ const createElements = () => {
             img.setAttribute('src', `${projects[key][index].src}`);
             img.setAttribute('alt', `${projects[key][index].name}`)
             div.appendChild(img);
+
 
             const a = document.createElement('a');
             a.setAttribute('src', '#');
@@ -100,13 +167,56 @@ const createElements = () => {
             panel.appendChild(a);
             panel.appendChild(a2);
             panel.appendChild(button);
-
             div.appendChild(panel)
-
         });
     }
-
 }
 createElements()
 
 
+const profileA = [...document.querySelectorAll('aside .projects a:first-of-type')];
+const profileA2 = [...document.querySelectorAll('aside .projects a:last-of-type')];
+const figures = [...document.querySelectorAll('.portfolio figure')];
+const myProjects = [...document.querySelectorAll('.portfolio .projects')];
+
+for (let img of profileA) {
+    const i = document.createElement('i');
+    i.classList.add('fas');
+    i.classList.add('fa-search');
+    img.before(i);
+}
+for (let img of profileA2) {
+    const i = document.createElement('i');
+    i.classList.add('fas');
+    i.classList.add('fa-code');
+    img.before(i);
+}
+
+const changeSelectedProjectNav = () => {
+    const selectedFigure = figures.findIndex(figure =>
+        figure.classList.contains('selected')
+    );
+    figures[selectedFigure].classList.remove('selected');
+}
+
+const changeSelectedProjects = () => {
+    const selectedProject = myProjects.findIndex(project =>
+        project.classList.contains('selected')
+    );
+    myProjects[selectedProject].classList.remove('selected');
+    const test = figures.findIndex(figure =>
+        figure.classList.contains('selected')
+    );
+    myProjects[test].classList.add('selected');
+}
+
+const navClicked = function (e) {
+    changeSelectedProjectNav();
+    this.classList.add('selected');
+    changeSelectedProjects();
+}
+
+
+figures.forEach(figure => {
+    figure.addEventListener('click', navClicked)
+})
